7d1c87750ee44f85d85f3d4a395e6a0cbebe8ee0
martinsaulo <martinsaulo77@gmail.com>
2023-09-12 20:24:12 -0300

    12/09

---
+++ MiLibroDeRecetas/Back/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Ingrediente> Ingredientes { get; set; }
+        public DbSet<Etiqueta> Etiquetas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=GABRIEL\\SQLEXPRESS;database=PruebaEF;trusted_connection=true;Encrypt=False");
+        }
+    }
+}
