1fe93c184287c7c210446e082eb4fb61d76bef17
martinsaulo <martinsaulo77@gmail.com>
2023-09-18 17:47:45 -0300

    Añadida la ultima migración

---
+++ MiLibroDeRecetas/Back/Migrations/20230918204538_Migracion02.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Back.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracion02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IngredientesReceta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ingrediente_Id = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientesReceta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IngredientesReceta_Ingredientes_Ingrediente_Id",
+                        column: x => x.Ingrediente_Id,
+                        principalTable: "Ingredientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientesReceta_Ingrediente_Id",
+                table: "IngredientesReceta",
+                column: "Ingrediente_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientesReceta");
+        }
+    }
+}
